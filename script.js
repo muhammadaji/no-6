8e584ca7e5a42e62206d2fc180797cd82118bb75
Muhammad Aji Raharjo <loroyangtau@gmail.com>
2019-02-15 22:29:07 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+main = (email) => {
+    if (/\S+@\S+\.\S+/.test(email) == true ) {
+        console.log("Berisi alamat surel");
+        console.log(email.substring(0));
+    }else{
+        console.log("Tidak berisi alamat surel");
+    }
+}
+
+main('dashgdy27t4@gmail.com');
