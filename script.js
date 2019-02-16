fdc010bc7d075c7ecad6069466fd89d4f9abb2a5
Muhammad Aji Raharjo <loroyangtau@gmail.com>
2019-02-16 12:45:02 +0700

    Update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 main = (email) => {
     if (/\S+@\S+\.\S+/.test(email) == true ) {
         console.log("Berisi alamat surel");
-        console.log(email.substring(0));
+        console.log(email.match(/\S+@\S+\.\S+/img).toString());
     }else{
         console.log("Tidak berisi alamat surel");
     }
 }
 
-main('dashgdy27t4@gmail.com');
+main('ini email saya dashgdy27t4@gmail.com atau ysa5de2g@gmail.com');
