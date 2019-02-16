3d9fe4070fbbf1f3db2ea52b1f31786d9cbbd3a2
Muhammad Aji Raharjo <loroyangtau@gmail.com>
2019-02-16 19:11:26 +0700

    fix code

--- script.js
+++ script.js
@@ -7,9 +7,4 @@ main = (email) => {
     }
 }
 
-<<<<<<< HEAD
 main('ini email saya dashgdy27t4@gmail.com atau ysa5de2g@gmail.com');
-=======
-main('dashgdy27t4@gmail.com');
-main('kulineri kuy');
->>>>>>> c86c1f03efa959e5f984a58ff42353ef0386c6d4
